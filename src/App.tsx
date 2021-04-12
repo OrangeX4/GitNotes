93a3a4fae76e2a73b57ac9fd7d48a91f05071d89
OrangeX4 <318483724@qq.com>
2021-04-12 23:56:09 +0800

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles'
 
 import './App.css'
@@ -50,11 +52,13 @@ const useStyles = makeStyles((theme: Theme) =>
             flexGrow: 1,
             padding: theme.spacing(3),
         },
+        github: {
+            '& > *': {
+                marginRight: theme.spacing(2),
+            },
+        }
     }),
 )
-
-interface Props {
-}
 
 function isMarkdown(name: string) {
     if (name.length >= 3 && name.slice(name.length - 3, name.length) === '.md') {
@@ -164,7 +168,7 @@ function contentProcess(content: string, file: File | null): string {
         }
         newContent += content.slice(start, content.length)
         content = newContent
-        
+
         // Html Image
         const imgExpression = /<img +src="([\u4e00-\u9fa5a-z0-9-_/. ]*)"([^>]*)>/g
         start = 0
@@ -197,7 +201,7 @@ function getQuery(): Query {
             result[pair[0]] = pair[1]
         }
     }
-    if (result.git !== 'github' && result.git !== 'gitlab') {
+    if (result.git !== 'github' && result.git !== 'gitlab' && result.git !== undefined) {
         result.git = 'github'
     }
     if (result.git === 'gitlab') {
@@ -213,8 +217,9 @@ function getQuery(): Query {
     return result
 }
 
+const host = 'http://localhost:3000/'
 
-export default function App(props: Props) {
+export default function App() {
     const classes = useStyles()
     const theme = useTheme()
     const [mobileOpen, setMobileOpen] = React.useState(false)
@@ -265,11 +270,18 @@ export default function App(props: Props) {
     }
 
     const [currentFile, setCurrentFile] = useState(null as File | null)
-    const [title, setTitle] = useState('标题')
-    const [content, setContent] = useState('# 请选择一个 Markdown 文件打开')
+    const [title, setTitle] = useState('OrangeX4\'s Notes')
+    const [content, setContent] = useState('')
 
     const [freshCount, setFreshCount] = useState(0)
     const fresh = () => setFreshCount(freshCount + 1)
+
+
+    const [githubRepo, setGithubRepo] = useState('OrangeX4/NJUAI-Notes')
+    const [githubToken, setGithubToken] = useState('')
+    const [gitlabHost, setGitlabHost] = useState('git.nju.edu.cn')
+    const [gitlabId, setGitlabId] = useState('2047')
+
 
     const drawer = (
         <div>
@@ -280,7 +292,11 @@ export default function App(props: Props) {
                 </Typography>
             </div>
             <Divider />
-            <SubList folder={folder} fresh={fresh} onClick={handleFileClick} onLoad={handleLoad} />
+            {getQuery().git ? (
+                <SubList folder={folder} fresh={fresh} onClick={handleFileClick} onLoad={handleLoad} />
+            ) : (
+                null
+            )}
         </div>
     )
 
@@ -335,7 +351,46 @@ export default function App(props: Props) {
             </nav>
             <main className={classes.content}>
                 <div className={classes.toolbar} />
-                <div dangerouslySetInnerHTML={{ __html: md.render(contentProcess(content, currentFile)) }} />
+                {getQuery().git ? (
+                    <div dangerouslySetInnerHTML={{ __html: md.render(contentProcess(content, currentFile)) }} />
+                ) : (
+                    <div>
+                        <h1>这是什么?</h1>
+                        <p>这是一个由 <b>OrangeX4</b> 开发的<b>笔记浏览应用</b>, 用于浏览以 <b>Markdown</b> 书写的, 存放在 <b>GitLab 或 GitHub</b> 上的笔记.</p>
+                        <p>优点: <b>数学公式支持和移动端适配.</b></p>
+                        <a href="https://github.com/OrangeX4/NJUAI-Notes">GitHub 地址</a>
+
+                        <h1>OrangeX4 的笔记</h1>
+                        <h2><a href={host + "?git=gitlab"}>OrangeX4's Notes</a></h2>
+                        <h1>其他笔记</h1>
+                        <h2><a href={host + "?git=github&github=typoverflow/note"}>Typoverflow's Notes</a></h2>
+                        <h2><a href={host + "?git=github&github=fengdu78/Coursera-ML-AndrewNg-Notes"}>Coursera-ML-AndrewNg-Notes</a></h2>
+
+                        <h1>GitHub</h1>
+                        <form className={classes.github} noValidate autoComplete="off">
+                            <TextField id="standard-basic" value={githubRepo} onChange={(e) => setGithubRepo(e.target.value)} label="Repo" />
+                            <TextField id="standard-basic" value={githubToken} onChange={(e) => setGithubToken(e.target.value)} label="Token" />
+                            <Button variant="outlined"
+                                color="primary"
+                                onClick={() => { window.location.href = `${host}?git=github&github=${githubRepo}${githubToken === '' ? '' : '&token=' + githubToken}` }}
+                                style={{ height: 50, width: 100, fontSize: 16 }}>浏览</Button>
+                        </form>
+                        <p>GitHub 对于 API 访问有一定的限制, 超过次数便需要 Token 才能继续浏览.</p>
+                        <p>详见 <a href="https://docs.github.com/cn/github/authenticating-to-github/creating-a-personal-access-token">创建 Token</a>.</p>
+
+                        <h1>GitLab</h1>
+                        <form className={classes.github} noValidate autoComplete="off">
+                            <TextField id="standard-basic" value={gitlabHost} onChange={(e) => setGitlabHost(e.target.value)} label="Host" />
+                            <TextField id="standard-basic" value={gitlabId} onChange={(e) => setGitlabId(e.target.value)} label="Id" />
+                            <Button variant="outlined"
+                                color="primary"
+                                onClick={() => { window.location.href = `${host}?git=gitlab&gitlab=${gitlabHost}&id=${gitlabId}` }}
+                                style={{ height: 50, width: 100, fontSize: 16 }}>浏览</Button>
+                        </form>
+                        <p>Host 是 GitLab 所在的域名, 如 "gitlab.com" 和 "git.nju.edu.cn".</p>
+                        <p>Id 是你要浏览的 GitLab 项目的 Id, 可以在项目页面上找到.</p>
+                    </div>
+                )}
             </main>
         </div>
     )
